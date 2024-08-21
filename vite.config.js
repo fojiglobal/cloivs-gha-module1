f53f9647032df63120d57cd611a5f815e2e76df0
Clovis Foji <nkempu20@yahoo.com>
2024-08-21 07:05:18 -0400

    Fixed Dependency file and break vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), eslint()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'jsdom'
     setupFiles: './src/test/setup.js',
   },
 });
